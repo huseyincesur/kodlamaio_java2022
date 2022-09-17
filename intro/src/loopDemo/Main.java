3fe9adc7c11165f678b809c44db2da8d0d228541
Hüseyin CESUR <huseyincesur@gmail.com>
2022-09-17 23:45:23 +0300

    loops-for_ders11_while_ders12_do-while_ders13

--- intro/src/loopDemo/Main.java
+++ intro/src/loopDemo/Main.java
@@ -9,6 +9,28 @@ public class Main {
 			System.out.println(i);
 		}
 		System.out.println("Döngü Bitti");
+		
+		//While
+		int i=2;
+		while(i<10) {
+			System.out.println(i);
+			//i++;
+			i+=2;
+		}
+		System.out.println("While Döngüsü Bitti");
+		
+		//infinite loop : sonsuz döngü demektir
+		
+		//Do-While döngüsü
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;
+		} while(j<10);
+		System.out.println("Do-While Döngüsü Bitti");
+		//Şart uymasa bile döngü en az 1 kere çalışır
+		//Döngü çalışmasa da yapılmak istenen bir operasyon varsa yapmak için kullanılır.
+		//Örneğin log atma
 	}
 
 }
