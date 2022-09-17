c088bb0f9f1e932ddc403ee3659340e5ac3334d5
Hüseyin CESUR <huseyincesur@gmail.com>
2022-09-17 22:35:52 +0300

    variables_ders6 & dataTypes_ders7 eklendi

---
+++ intro/src/dataTypes/Main.java
@@ -0,0 +1,28 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// type safe
+		
+		/*
+		 * Primitive Types
+		 * 	boolean	true or false 		byte
+		 * 	char	single character	2 bytes
+		 * 	byte	integer				byte		-128 to 127
+		 * 	short	integer				2 bytes		-32768 to 32767
+		 * 	int		integer				4 bytes
+		 * 	long	integer				8 bytes
+		 * 	float	floating point		4 bytes
+		 * 	double	floating point		8 bytes
+		 */
+		
+		boolean dogruMu = false;
+		char katakter ='A';
+		byte sayiByte = 127;
+		short sayiShort = 12345;
+		int sayiInt = 1234567890;
+		
+	}
+
+}
---
+++ intro/src/javaVariables/Main.java
@@ -0,0 +1,19 @@
+package javaVariables;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi=12;
+		String mesaj ="Öğrenci Sayısı: ";
+
+		System.out.println("Öğrenci Sayım: "+ogrenciSayisi);
+		System.out.println("Öğrenci Sayım: "+ogrenciSayisi);
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		
+		//reusability : 1 kere yaz ve tekrer tekrar kullan
+		//code refactoring : kirli kodları temizleme
+		
+	}	
+}
