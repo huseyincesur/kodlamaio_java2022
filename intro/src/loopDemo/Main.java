42fde3bd7bf67db881eaba15b1c190f1c20f9c3a
Hüseyin CESUR <huseyincesur@gmail.com>
2022-09-17 23:44:02 +0300

    forLoop_ders11 while_ders12 do-while_ders13

---
+++ intro/src/loopDemo/Main.java
@@ -0,0 +1,14 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//for loop
+		for (int i = 2; i < 10; i+=2) {
+			//i++ birer artırır
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+	}
+
+}
